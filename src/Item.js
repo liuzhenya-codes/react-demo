46337dc3a70c92f415a9683e12cd0f333968c808
Liu, Zhenya <liuzhenya@three-geeks.com>
2021-03-12 08:41:24 +0000

    React 中的 props 的类型校验和默认值设置

--- src/Item.js
+++ src/Item.js
@@ -1,4 +1,5 @@
 import { Component, Fragment } from 'react'
+import ProsTypes from 'prop-types'
 
 class Item extends Component {
   constructor (props) {
@@ -11,11 +12,26 @@ class Item extends Component {
   render () {
     return (
       <Fragment>
-        <li>{this.props.text}</li>
+        <li>{`${this.props.test} - ${this.props.text}`}</li>
         <button onClick={this.handelDeleteItem}>删除</button>
       </Fragment>
     )
   }
+}
+
+/*
+React 中的 props 类型和默认值
+文档: https://react.docschina.org/docs/typechecking-with-proptypes.html
+*/
+Item.propTypes = { // 配置 props 里各项的类型
+  test: ProsTypes.string.isRequired,
+  content: ProsTypes.oneOfType([ProsTypes.number, ProsTypes.string]),
+  index: ProsTypes.number,
+  handelDeleteItem: ProsTypes.func
+}
+
+Item.defaultProps = { // 配置 props 里各项的默认值
+  test: '默认值'
 }
 
 export default Item
