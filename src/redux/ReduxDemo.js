667256a6ea8601d919ce5188112052f5c5f596f8
Liu, Zhenya <liuzhenya@three-geeks.com>
2021-03-14 09:33:56 +0000

    redux 中 store 的修改和监听

--- src/redux/ReduxDemo.js
+++ src/redux/ReduxDemo.js
@@ -8,22 +8,53 @@ class ReduxDemo extends Component {
     console.log(this.state)
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handelInputKeyUp = this.handelInputKeyUp.bind(this)
+    store.subscribe(this.handleStoreChange)
   }
 
-  handleInputChange () {}
+  handleInputChange (e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
 
-  handleBtnClick () {}
+  handleBtnClick () {
+    const action = {
+      type: 'add_list'
+    }
+    store.dispatch(action)
+    this.handleInputChange({target: {value: ''}})
+  }
+
+  handelInputKeyUp (e) {
+    if (e.keyCode === 13) this.handleBtnClick()
+  }
+
+  handelDelete (index) {
+    const action = {
+      type: 'delete_list',
+      index
+    }
+    store.dispatch(action)
+  }
+
+  handleStoreChange () {
+    this.setState(store.getState())
+  }
 
   render () {
     return (
       <div>
-        <input value={this.state.inputValue} onChange={this.handleInputChange}/>
+        <input value={this.state.inputValue} onChange={this.handleInputChange} onKeyUp={this.handelInputKeyUp}/>
         <button onClick={this.handleBtnClick}>提交</button>
         <ul>
           {
             this.state.list.map((item, index) => {
               return (
-                <li key={item}>{item}</li>
+                <li key={item} onClick={this.handelDelete.bind(this, index)}>{item}</li>
               )
             })
           }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,28 @@
 const stateData = {
-  inputValue: '123',
-  list: [
-    'a',
-    'b'
-  ]
+  inputValue: '',
+  list: []
 }
 
 const reducer = (state = stateData, action) => {
+  switch (action.type) {
+    case 'change_input_value': {
+      const newState = JSON.parse(JSON.stringify(state))
+      newState.inputValue = action.value
+      return newState
+    }
+    case 'add_list': {
+      const newState = JSON.parse(JSON.stringify(state))
+      newState.list.push(newState.inputValue)
+      return newState
+    }
+    case 'delete_list': {
+      const newState = JSON.parse(JSON.stringify(state))
+      newState.list.splice(action.index, 1)
+      return newState
+    }
+    default:
+      break
+  }
   return state
 }
 
